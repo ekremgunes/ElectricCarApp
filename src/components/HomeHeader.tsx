0f282842d34d8fa23e6f58ffdd5bd2cbc4d5f615
ekremgunes <github1357@gmail.com>
2024-11-08 21:48:17 +0300

    Icon changed

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -2,13 +2,13 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import Avatar from './Avatar';
-import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
+import Feather from '@expo/vector-icons/Feather';
 
 const HomeHeader = () => {
     return (
         <View className=' flex-row px-6 items-center justify-between w-full'>
             <TouchableOpacity>
-                <SimpleLineIcons name="menu" size={26} color="black" />
+                <Feather name="menu" size={28} color="black" />
             </TouchableOpacity>
             <Avatar></Avatar>
         </View>
